c2570613628a5ebf2697e497e18218d9dd5e7554
Bedugam kalyan Kumar <81046499+kalyan1421@users.noreply.github.com>
2023-03-23 10:23:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 const DBconnection = require('./db')
 app.use(express.json())
 
